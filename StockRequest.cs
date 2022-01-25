a1aae91bc1c81c2d60ef0a68d581da6196b00d03
Joao Carvalho <carvalhojldc@gmail.com>
2022-01-25 01:13:01 -0300

    Add comments about API that will be used to request stock price
    Signed-off-by: Joao Carvalho <carvalhojldc@gmail.com>

--- StockRequest.cs
+++ StockRequest.cs
@@ -8,6 +8,20 @@ namespace StockRequestInterface
 {
     internal class StockRequest
     {
+        /*
+         * Using API from https://www.alphavantage.co/
+         *
+         * =========================================================================
+         * Question from https://www.alphavantage.co/support/#api-key
+         *
+         * Are there usage/frequency limits for the API service?
+         *
+         * We are pleased to provide free stock API service for our global community \
+         * of users for up to 5 API requests per minute and 500 requests per day. \
+         * If you would like to target a larger API call volume, please \
+         * visit premium membership.
+         */
+
         public StockRequest()
         {
 
@@ -15,6 +29,12 @@ namespace StockRequestInterface
 
         public float GetPrice(string stockName)
         {
+            /*
+             * Quote Endpoint Trending
+             *
+             * A lightweight alternative to the time series APIs, this service returns the
+             * price and volume information for a token of your choice
+             */
             return 12;
         }
     }
