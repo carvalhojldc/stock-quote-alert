fb7673fa47d1e0dcabcf9c1ee459fe0f19cbc759
Joao Carvalho <carvalhojldc@gmail.com>
2022-01-25 15:46:56 -0300

    Integrate email notification with stock monitor

--- StockMonitor.cs
+++ StockMonitor.cs
@@ -1,4 +1,5 @@
-﻿using StockRequestInterface;
+﻿using MailInterface;
+using StockRequestInterface;
 
 namespace StockMonitorInterface
 {
@@ -7,8 +8,18 @@ namespace StockMonitorInterface
         private readonly double priceToSell, priceToBuy;
         private readonly string stockName;
         private readonly StockRequest stockRequest;
+        private readonly Mail mail;
         private double currentPrice;
 
+        enum QuoteStatus
+        {
+            Stay,
+            Buy,
+            Sell
+        };
+
+
+        private QuoteStatus quoteStatus = QuoteStatus.Stay;
         public StockMonitor(string stockName, double priceToSell, double priceToBuy)
         {
             Console.WriteLine("\nStock name: " + stockName +
@@ -19,6 +30,7 @@ namespace StockMonitorInterface
             this.priceToBuy = priceToBuy;
             this.stockName = stockName;
             this.stockRequest = new StockRequest();
+            this.mail = new Mail();
         }
 
         public void Monitor()
@@ -30,12 +42,42 @@ namespace StockMonitorInterface
 
                 if (this.currentPrice >= this.priceToSell)
                 {
-                    Console.WriteLine("Time to sell!");
+                    if (quoteStatus != QuoteStatus.Sell)
+                    {
+                        quoteStatus = QuoteStatus.Sell;
+
+                        string subject = "It's time to sell " + this.stockName;
+                        string msg = "Hello,\n\n" +
+                            "It's time to sell " + this.stockName + ".\n" +
+                            "Stock quote: R$ " + this.currentPrice + ".\n\n" +
+                            "Att,\nStock Quote Alert.";
+
+                        Console.WriteLine(subject);
+                        this.mail.Send(subject, msg);
+                    }
                 }
                 else if (this.currentPrice <= this.priceToBuy)
                 {
-                    Console.WriteLine("Time to buy!");
+                    if (quoteStatus != QuoteStatus.Buy)
+                    {
+                        quoteStatus = QuoteStatus.Buy;
+
+                        string subject = "It's time to buy " + this.stockName;
+                        string msg = "Hello,\n\n" +
+                            "It's time to buy " + this.stockName + ".\n" +
+                            "Stock quote: R$ " + this.currentPrice + ".\n\n" +
+                            "Att,\nStock Quote Alert.";
+
+                        Console.WriteLine(subject);
+                        this.mail.Send(subject, msg);
+                    }
                 }
+                else
+                {
+                    quoteStatus = QuoteStatus.Stay;
+                }
+
+                Console.WriteLine(".");
 
                 Thread.Sleep(15000);
             }
